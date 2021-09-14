9af560ce7127b958b4a01fcc5819e588a92ce354
Abhishek Yadav <abhishek.yadav.17@mountblue.tech>
2021-09-14 13:49:52 +0530

    comments removed

--- callback3.js
+++ callback3.js
@@ -1,15 +1,3 @@
-// const data = require('./jsonfiles/cards.json');
-
-// function callback3 (id, cb){
-//     setTimeout(() => {
-//         const result = cb(id, data);
-//         console.log('3',result);
-//         return result;
-//     }, 2 * 1000);
-// }
-
-// module.exports = callback3;
-
 function callback3 (id, data, cb){
     setTimeout(() => {
         let result = Object.entries(data).find(element => element[0] === id);
