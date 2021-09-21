e1217b0a47f94833efd2289cd06c04e56d1be237
Abhishek Yadav <abhishek.yadav.17@mountblue.tech>
2021-09-21 12:04:42 +0530

    Added Qeustion to callback files

--- callback2.js
+++ callback2.js
@@ -1,3 +1,9 @@
+/* 
+	Problem 2: Write a function that will return all lists that belong to a board based on 
+    the boardID that is passed to it from the given data in lists.json. 
+    Then pass control back to the code that called it by using a callback function.
+*/
+
 function callback2 (id, data, cb){
     setTimeout(() => {
         let result = Object.entries(data).find(element => element[0] === id);
