c5625f7254669103d560175a8409067a570c0a1a
Abhishek Yadav <abhishek.yadav.17@mountblue.tech>
2021-09-21 12:01:55 +0530

    Added Questions to callback files

--- callback2.js
+++ callback2.js
@@ -1,10 +1,8 @@
-// function callback2 (id, data, cb){
-//     setTimeout(() => {
-//         let result = Object.entries(data).find(element => element[0] === id);
-//         let err = new Error("Data Not Found");
-//         result ? cb(null, result) : cb(err);
-//     }, 2 * 1000);
-// }
+/* 
+	Problem 2: Write a function that will return all lists that belong to a board based on 
+    the boardID that is passed to it from the given data in lists.json. 
+    Then pass control back to the code that called it by using a callback function.
+*/
 
 function callback2 (id, data){
     return new Promise((resolve, reject)=> {
--- callback6.js
+++ callback6.js
@@ -1,29 +1,14 @@
+/* 
+	Problem 6: Write a function that will use the previously written functions to get the following information. 
+    You do not need to pass control back to the code that called it.
+
+    Get information from the Thanos boards
+    Get all the lists for the Thanos board
+    Get all cards for all lists simultaneously
+*/
+
 function callback6(callback1, callback2, callback3, boards, lists, cards){ 
     setTimeout(() => {
-        // callback1("Thanos", boards, (err, result) => {
-        //     if (result){
-        //         console.log(result);
-        //         callback2(result.id, lists, (err, result) => {
-        //             if (result){
-        //                 console.log(result);
-        //                 result[1].forEach(element => {
-        //                     console.log(element.id);
-        //                     callback3(element.id, cards, (err, result) => {
-        //                         if (result){
-        //                             console.log(result);
-        //                         }else{
-        //                             console.log(err);
-        //                         }
-        //                     });
-        //                 });   
-        //             }else{
-        //                 console.log(err);
-        //             }
-        //         });
-        //     }else{
-        //         console.log(err);
-        //     }
-        // });
         callback1("Thanos", boards)
         .then((res)=> {
             console.log(res);
